d12ecee443f22367e64a726615b71c24e601df86
dragneellaplace <dragneellaplace@gmail.com>
2026-09-24 21:40:22 -0300

    config inicial

---
+++ processador.py
@@ -0,0 +1,11 @@
+print("Processador iniciado!")
+
+def soma(a, b):
+    # Hotfix: corrige erro de tipo de dado na soma (conversão para float)
+    return float(a) + float(b)
+
+def divide(a, b):
+    # Proteção contra divisão por zero
+    if float(b) == 0.0:
+        return "Erro: Divisão por zero!"
+    return float(a) / float(b)
