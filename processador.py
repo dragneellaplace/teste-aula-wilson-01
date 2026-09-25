9f3dd9bbc1befd5c3a4eec1b00d7fe84f09cd79a
dragneellaplace <dragneellaplace@gmail.com>
2026-09-24 22:06:27 -0300

    resolvendo merge

--- processador.py
+++ processador.py
@@ -2,11 +2,8 @@ print("Processador iniciado!")
 
 def soma(a, b):
     # Hotfix: corrige erro de tipo de dado na soma (conversão para float)
-<<<<<<< HEAD
+
     return float(a) + float(b) # comentário diferente
-=======
-    return float(a) + float(b) #comentário completamente inutil
->>>>>>> experimento
 
 def divide(a, b):
     # Proteção contra divisão por zero
